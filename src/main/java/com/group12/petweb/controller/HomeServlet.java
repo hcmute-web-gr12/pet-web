beb3fc2391b2fce77602ec2fefc354fd339edad0
g-ddy <giddygeedy12@gmail.com>
2022-10-26 01:14:25 +0700

    Add HomeServlet as welcome file.

--- src/main/java/com/group12/petweb/controller/HomeServlet.java
+++ src/main/java/com/group12/petweb/controller/HomeServlet.java
@@ -1,13 +1,16 @@
 package com.group12.petweb.controller;
 
 import java.io.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "Home", value = "/")
-public class HelloServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    }
-    public void destroy() {
+@WebServlet(name = "homeServlet", value = "/home")
+public class HomeServlet extends HttpServlet {
+    @Override()
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/Home.jsp");
+        dispatcher.forward(request, response);
     }
 }
