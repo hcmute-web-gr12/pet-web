ee76d04c65066d215a0261ffe4bf8fe619adff4d
g-ddy <giddygeedy12@gmail.com>
2022-10-26 01:16:53 +0700

    Rename to HomeController.

--- src/main/java/com/group12/petweb/controller/HomeController.java
+++ src/main/java/com/group12/petweb/controller/HomeController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "homeServlet", value = "/home")
-public class HomeServlet extends HttpServlet {
+public class HomeController extends HttpServlet {
     @Override()
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/Home.jsp");
