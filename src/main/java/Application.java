01dcb914dc1f54fc19fe0ac4da90f6fdd70aa6b8
g-ddy <giddygeedy12@gmail.com>
2022-11-09 21:28:28 +0700

    Implement HTTP interfaces.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,20 +2,55 @@ import com.group12.petweb.controller.HomeController;
 import com.group12.petweb.controller.LoginController;
 import com.group12.petweb.controller.ProductsController;
 import com.group12.petweb.controller.SignUpController;
+import com.group12.petweb.service.HibernateSessionFactory;
+import com.group12.petweb.service.HibernateSessionFactoryImpl;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
 @WebListener
-public class Application implements ServletContextListener {
+public class Application implements ServletContextListener, HttpSessionListener, HttpSessionAttributeListener {
+    private final HibernateSessionFactory hibernateSessionFactory;
+    public Application() {
+        hibernateSessionFactory = new HibernateSessionFactoryImpl();
+    }
+
     @Override
-    public void contextInitialized(ServletContextEvent event) {
-        ServletContext context = event.getServletContext();
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
         context.addServlet("homeServlet", new HomeController()).addMapping("/home");
-        context.addServlet("loginServlet", new LoginController()).addMapping("/login");
+        context.addServlet("loginServlet", new LoginController(hibernateSessionFactory)).addMapping("/login");
         context.addServlet("signUpServlet", new SignUpController()).addMapping("/signup");
         context.addServlet("productsServlet", new ProductsController()).addMapping("/products");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+    }
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent sbe) {
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent sbe) {
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent sbe) {
     }
 }
