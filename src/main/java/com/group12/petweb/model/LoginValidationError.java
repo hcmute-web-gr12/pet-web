4e1ed51aea205f49884ef629e7e5159b300b169c
g-ddy <giddygeedy12@gmail.com>
2022-11-11 21:02:50 +0700

    Add LoginValidationError model.

---
+++ src/main/java/com/group12/petweb/model/LoginValidationError.java
@@ -0,0 +1,19 @@
+package com.group12.petweb.model;
+
+public class LoginValidationError {
+    private String email;
+    private String password;
+
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+
+    @Override
+    public String toString() {
+        return "LoginValidationError{" +
+                "email='" + getEmail() + '\'' +
+                ", password='" + getPassword() + '\'' +
+                '}';
+    }
+}
