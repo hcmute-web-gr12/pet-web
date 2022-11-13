61a3d902b4adc394bcc59c393422cff9e5f743b6
g-ddy <giddygeedy12@gmail.com>
2022-11-13 17:53:40 +0700

    Add SignUpValidationError model.

---
+++ src/main/java/com/group12/petweb/model/SignUpValidationError.java
@@ -0,0 +1,26 @@
+package com.group12.petweb.model;
+
+public class SignUpValidationError {
+    private String username;
+    private String email;
+    private String password;
+    private String passwordVerify;
+
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getPasswordVerify() { return password; }
+    public void setUsername(String username) { this.username = username; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public void setPasswordVerify(String passwordVerify) { this.passwordVerify = passwordVerify; }
+
+    @Override
+    public String toString() {
+        return "SignUpValidationError{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", passwordVerify='" + passwordVerify + '\'' +
+                '}';
+    }
+}
