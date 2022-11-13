d2fb2ebc557f03257df8636fb77a211c939d5f00
g-ddy <giddygeedy12@gmail.com>
2022-11-13 17:54:09 +0700

    Remove parameterized constructor.

--- src/main/java/com/group12/petweb/model/User.java
+++ src/main/java/com/group12/petweb/model/User.java
@@ -13,12 +13,6 @@ public class User {
     private String email;
     private String password;
 
-    public User(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
     public User() {
     }
 
