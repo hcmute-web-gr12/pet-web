0958eb836831e1c0fd1fa52e150a554695a943e0
g-ddy <giddygeedy12@gmail.com>
2022-11-21 00:48:55 +0700

    Add js-script submodule.

---
+++ src/main/webapp/scripts/userProfile.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cash_dom_1 = __importDefault(require("cash-dom"));
+(0, cash_dom_1.default)('h1').text('OK');
