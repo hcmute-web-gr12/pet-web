4e08e696c2f6934633026321bce656b294e3f00f
g-ddy <giddygeedy12@gmail.com>
2022-12-07 00:40:56 +0700

    Add generate pet image url overloads.

--- src/main/java/com/group12/petweb/util/CloudinaryUtils.java
+++ src/main/java/com/group12/petweb/util/CloudinaryUtils.java
@@ -1,7 +1,15 @@
 package com.group12.petweb.util;
 
+import java.util.function.Consumer;
+
+import com.cloudinary.Transformation;
+import com.cloudinary.Url;
 import com.group12.petweb.model.Pet;
 
 public interface CloudinaryUtils {
 	String generateImageUrl(Pet pet);
+
+	String generateImageUrl(Pet pet, Consumer<Url> urlConsumer);
+
+	String generateImageUrl(Pet pet, Consumer<Url> urlConsumer, Consumer<Transformation> transformationConsumer);
 }
--- src/main/java/com/group12/petweb/util/CloudinaryUtilsImpl.java
+++ src/main/java/com/group12/petweb/util/CloudinaryUtilsImpl.java
@@ -1,6 +1,10 @@
 package com.group12.petweb.util;
 
+import java.util.function.Consumer;
+
 import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.cloudinary.Url;
 import com.group12.petweb.model.Pet;
 
 public class CloudinaryUtilsImpl implements CloudinaryUtils {
@@ -18,5 +22,31 @@ public class CloudinaryUtilsImpl implements CloudinaryUtils {
 				.format(pet.getImageFormat())
 				.version(pet.getImageVersion())
 				.generate();
+	}
+
+	public String generateImageUrl(Pet pet, Consumer<Url> urlConsumer) {
+		final var url = cloudinary
+				.url()
+				.secure(true)
+				.publicId(pet.getImagePublicId())
+				.format(pet.getImageFormat())
+				.version(pet.getImageVersion());
+		urlConsumer.accept(url);
+		return url.generate();
+	}
+
+	public String generateImageUrl(Pet pet, Consumer<Url> urlConsumer,
+			Consumer<Transformation> transformationConsumer) {
+		final var url = cloudinary
+				.url()
+				.secure(true)
+				.publicId(pet.getImagePublicId())
+				.format(pet.getImageFormat())
+				.version(pet.getImageVersion());
+		urlConsumer.accept(url);
+		final var transformation = new Transformation<>();
+		transformationConsumer.accept(
+				transformation);
+		return url.transformation(transformation).generate();
 	}
 }
